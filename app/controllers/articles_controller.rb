981320576645487b547809baf8d0b3711f10f94a
gaurav <gk.raj953435@gmail.com>
2024-03-19 23:08:43 +0530

    Add users create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,6 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.user= User.first
-    @article.save
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article 
