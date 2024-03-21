92c9ef28d58a619d9d83f04c5a4eafc0477d0f7b
gaurav <gk.raj953435@gmail.com>
2024-03-21 23:48:03 +0530

    category model and testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_21_145617) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_21_181246) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
