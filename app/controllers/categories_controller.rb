75aebb872e269c0cdb4a61619c0b42d3dd1fa973
rajgaurav95 <gk.raj.953435@gmail.com>
2024-03-22 23:56:11 +0530

    edit categories and section completed

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController  < ApplicationController
  
   before_action :require_admin, except: [:index, :show]
+
   def new
     @category=Category.new
   end
@@ -22,6 +23,20 @@ class CategoriesController  < ApplicationController
   def show
     @category=Category.find(params[:id])
     @articles= @category.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def edit
+    @category=Category.find(params[:id])
+  end
+
+  def update
+    @category=Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice]="Category name updated successfully"
+      redirect_to @category
+    else
+      render 'edit',  status: :unprocessable_entity
+    end
   end
 
   private
